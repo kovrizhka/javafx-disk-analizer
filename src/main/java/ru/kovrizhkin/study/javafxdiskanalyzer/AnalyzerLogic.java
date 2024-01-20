3a606f8171883202e8c71c5ab48bc5c1c9cb6e3b
kovrizhka <pivsru@gmail.com>
2024-01-20 13:38:46 +0500

    AnalyzerLogic class

---
+++ src/main/java/ru/kovrizhkin/study/javafxdiskanalyzer/AnalyzerLogic.java
@@ -0,0 +1,54 @@
+package ru.kovrizhkin.study.javafxdiskanalyzer;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AnalyzerLogic {
+
+    private HashMap<String, Long> sizes;
+
+    private Map<String, Long> calculateDirectorySize(Path path) {
+        try {
+             sizes = new HashMap<>();
+            Files.walkFileTree(
+                    path,
+                    new SimpleFileVisitor<>() {
+                        @Override
+                        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                            long size = Files.size(file);
+                            updateDirectorySize(file, size);
+                            return super.visitFile(file, attrs);
+                        }
+
+                        @Override
+                        public FileVisitResult visitFileFailed(Path file, IOException exc) {
+                            return FileVisitResult.SKIP_SUBTREE;
+                        }
+                    });
+
+            return sizes;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void updateDirectorySize(Path file, Long size) {
+        String key = file.toString();
+        sizes.put(key, size + sizes.getOrDefault(key, 0L));
+
+        Path parent = file.getParent();
+
+        if(parent != null) {
+            updateDirectorySize(file, size);
+        }
+
+    }
+
+}
